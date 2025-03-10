d42a86c28043710e620f25be28481a8dc4ad1063
Havishya Vally Sunkara <83769088+havishya10@users.noreply.github.com>
2025-03-10 18:06:14 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    host: "192.168.31.113",
-  },
   resolve: {
     alias: { util: "util/" },
   },
